d3f10eebe291cf419f224660dba1c383a65ca5ed
AlyssaD <pink_lime@live.com>
2022-12-21 14:17:10 -0500

    add player movement

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class Player {
       x: 0,
       y: 0
     }
+    this.rotation = 0
 
     const image = new Image()
     image.src = './img/spaceship.png'
@@ -27,19 +28,93 @@ class Player {
   draw() {
     // c.fillStyle = 'red'
     // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    if (this.image)
-      c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    if (this.image) {
+      c.save()
+      c.translate(
+        player.position.x + player.width / 2, 
+        player.position.y + player.height / 2
+      )
+      c.rotate(this.rotation)
+
+      c.translate(
+        -player.position.x - player.width / 2, 
+        -player.position.y - player.height / 2
+      )
+
+      c.drawImage(
+        this.image, 
+        this.position.x, 
+        this.position.y, 
+        this.width, 
+        this.height
+      )
+      c.restore()
+    }
+  }
+
+  update() {
+    if (this.image) {
+      this.draw()
+      this.position.x += this.velocity.x
+    }
   }
 }
 
 const player = new Player()
-player.draw()
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  space: {
+    pressed: false
+  },
+}
 
 function animate() {
   requestAnimationFrame(animate)
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
-  player.draw()
+  player.update()
+
+  if (keys.a.pressed && player.position.x >= 0) {
+    player.velocity.x = -5
+    player.rotation = -0.15
+  } else if (keys.d.pressed && player.position.x +player.width <= canvas.width) {
+    player.velocity.x = 5
+    player.rotation = 0.15
+  } else {
+    player.velocity.x = 0
+    player.rotation = 0
+  }
 }
 
 animate()
+
+addEventListener('keydown', ({key}) => {
+  switch (key) {
+    case 'a':
+      keys.a.pressed = true
+      break
+    case 'd':
+      keys.d.pressed = true
+      break
+    case ' ':
+      break
+  }
+})
+
+addEventListener('keyup', ({key}) => {
+  switch (key) {
+    case 'a':
+      keys.a.pressed = false
+      break
+    case 'd':
+      keys.d.pressed = false
+      break
+    case ' ':
+      break
+  }
+})
