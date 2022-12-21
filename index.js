8a8b03cb9cc3724e36cfe7bbdbc09e3d1117f0d0
AlyssaD <pink_lime@live.com>
2022-12-21 15:20:17 -0500

    add invader class

--- index.js
+++ index.js
@@ -82,8 +82,50 @@ class Projectile {
   }
 }
 
+class Invader {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0
+    }
+
+    const image = new Image()
+    image.src = './img/invader.png'
+    image.onload = () => {
+      this.image = image
+      this.width = image.width * 0.2
+      this.height = image.height * 0.2
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height / 2
+      }
+    }
+  }
+
+  draw() {
+    if (this.image) {
+      c.drawImage(
+        this.image, 
+        this.position.x, 
+        this.position.y, 
+        this.width, 
+        this.height
+      )
+    }
+  }
+
+  update() {
+    if (this.image) {
+      this.draw()
+      this.position.x += this.velocity.x
+      this.position.y += this.velocity.y
+    }
+  }
+}
+
 const player = new Player()
 const projectiles = []
+const invader = new Invader()
 const keys = {
   a: {
     pressed: false
@@ -100,6 +142,7 @@ function animate() {
   requestAnimationFrame(animate)
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
+  invader.update()
   player.update()
   projectiles.forEach((projectile, i) => {
     if (projectile.position.y + projectile.radius <= 0) {
