4be33db3f7e3dbf8152bab8d013cf76a9cfa57dd
AlyssaD <pink_lime@live.com>
2022-12-21 12:36:40 -0500

    add player class

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const c = canvas.getContext('2d')
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 200,
+      y: 200
+    }
+
+    this.velocity = {
+      x: 0,
+      y: 0
+    }
+
+    //this.image = 
+    this.width = 100
+    this.height = 100
+  }
+
+}
